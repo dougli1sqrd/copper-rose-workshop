862174e9f28dda8f1b6456eeb417e2717b1cb06c
Eric <dougli1sqrd@gmail.com>
2026-07-22 09:58:56 -0700

    update tutor copy, fixed copper rose logo pathing

--- src/components/copper-rose.jsx
+++ src/components/copper-rose.jsx
@@ -4,7 +4,7 @@ export const CopperRose = () => {
     return (
         <div className="w-32">
             <ReactSVG
-                src="./astro-medallion/copper-rose-32.svg"
+                src="./copper-rose-32.svg"
                 className="inline"
                 wrapper="span"
             />
